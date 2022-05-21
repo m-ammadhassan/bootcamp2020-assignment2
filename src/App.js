9e61ea6d28606c42a444142b232b5fbf84f11a84
Ammad Hassan <m.ammadhassan@yahoo.com>
2022-05-21 20:31:08 +0500

    Assignment completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="flex">
+        <h1>Assignment Title : <span>React Basic App</span></h1>
       </header>
-    </div>
+
+      <main className="flex">
+        <h4>Hello World by <span>Muhammad Ammad Hassan</span></h4>
+      </main>
+
+      <footer className="flex">
+        <div class="social-icons flex">
+          <a href="https://web.facebook.com/m_ammad_hassan/" target="_blank">
+            <i class="fab fa-facebook-square"></i>
+          </a>
+          <a href="https://twitter.com/m_ammadhassan" target="_blank">
+            <i class="fab fa-twitter-square"></i>
+          </a>
+          <a href="https://www.instagram.com/m_ammadhassan" target="_blank">
+            <i class="fab fa-instagram-square"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/m-ammadhassan/" target="_blank">
+            <i class="fab fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/m-ammadhassan" target="_blank">
+            <i class="fab fa-github-square"></i>
+          </a>
+        </div>
+      </footer>
+    </>
   );
 }
 
